c74dcbd4f8c6ec94dbe31621229108676187eacb
Sameer <meersj@gmail.com>
2024-06-14 18:03:41 +0545

    fixed 'key' issue

--- src/containers/Products/Products.tsx
+++ src/containers/Products/Products.tsx
@@ -1,18 +1,19 @@
-import CardWithImage from "@/components/elements/Card/CardThreeDImage";
-import TitleWithContent from "@/components/elements/TitleWithContent/TitleWithContent";
-import { products } from "./ProductsArray";
+import CardWithImage from '@/components/elements/Card/CardThreeDImage';
+import TitleWithContent from '@/components/elements/TitleWithContent/TitleWithContent';
+import { products } from './ProductsArray';
 
 const Products: React.FC = () => {
   return (
     <>
-      <TitleWithContent title="Products" />
-      <div className="flex flex-wrap justify-center gap-12">
+      <TitleWithContent title='Products' />
+      <div className='flex flex-wrap justify-center gap-12'>
         {products.map((product, index) => (
           <CardWithImage
             linkUrl={product.linkUrl}
             imageUrl={product.imageUrl}
             cardTitle={product.cardTitle}
             cardContent={product.cardContent}
+            key={index}
           />
         ))}
       </div>
